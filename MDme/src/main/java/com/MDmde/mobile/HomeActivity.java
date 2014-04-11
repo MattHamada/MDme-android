739558206fb116e761c19e9bcacc81e640042caf
MattHamada <mattahamada@gmail.com>
2014-04-11 12:22:08 -0700

    Added theme

--- MDme/src/main/java/com/MDmde/mobile/HomeActivity.java
+++ MDme/src/main/java/com/MDmde/mobile/HomeActivity.java
@@ -53,6 +53,7 @@ public class HomeActivity extends ActionBarActivity {
 
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
+        setTitle("Home");
         return true;
     }
 
