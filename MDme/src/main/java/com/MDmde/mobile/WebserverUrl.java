49d8fdc52dd032de07f3b8bc19d27edd2dfb980a
MattHamada <mattahamada@gmail.com>
2014-11-07 14:23:20 -0700

    GCM messages now working.  Now just have to customize them for use

--- MDme/src/main/java/com/MDmde/mobile/WebserverUrl.java
+++ MDme/src/main/java/com/MDmde/mobile/WebserverUrl.java
@@ -8,8 +8,8 @@ public class WebserverUrl
 
     //public static final String ROOT_URL = "http://10.0.2.2:3000"; //emulator
     //public static final String ROOT_URL = "https://www.mdme.us";  //production
-    //public static final String ROOT_URL = "http://www.mdme.tk:3000"; //usb tether
-    public static final String ROOT_URL = "http://www.mdme.ak:3000"; //home net
+    public static final String ROOT_URL = "http://www.mdme.tk:3000"; //usb tether
+    //public static final String ROOT_URL = "http://www.mdme.ak:3000"; //home net
 
 
 }
